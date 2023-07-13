553a6c4ea964a3c3574f16c07a2a5b2a81086e15
Mehdi Aghaei <MohammadMehdi.AghaeiHosseinabadi@mt.com>
2023-07-13 15:27:18 +0200

    Add project files.

---
+++ BuildingBlocks.Tests/Common/Validations/ValidationExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿// -------------------------------------------------------
+// Copyright © Coalition of the Good-Hearted Engineers
+// -------------------------------------------------------
+
+using BuildingBlocks.Common.Validations;
+
+namespace BuildingBlocks.Tests.Common;
+
+public class ValidationExtensionsTests
+{
+	[Fact]
+	public void Validate_ValidationPredicates_True()
+	{
+		// Arrange
+		IEnumerable<int> randomNumbers = GenrateRandomNumbers(10,30);
+		Func<IEnumerable<int>, bool> allPositive = (x) => x.All(x => x > 0);
+		Func<IEnumerable<int>, bool> allInRange = (x) => x.All(x => x < 100);
+		Func<IEnumerable<int>, bool> correctCount = (x) => x.Count() is 10;
+
+		// Act 
+		var result = randomNumbers.Validate(allPositive, allInRange, correctCount);
+
+		// Assert
+		result.Should().BeTrue();
+	}
+
+	private static int[] GenrateRandomNumbers(int length, int maxValue)
+	{
+		var random = new Random();
+		
+		return Enumerable.Range(1, length).Select(s => random.Next(maxValue))
+		.Where(a => a > 0).ToArray();
+	}
+}
---
+++ BuildingBlocks/Common/Validations/ValidationEtensions.cs
@@ -0,0 +1,11 @@
+﻿// -------------------------------------------------------
+// Copyright © Coalition of the Good-Hearted Engineers
+// -------------------------------------------------------
+
+namespace BuildingBlocks.Common.Validations;
+
+public static class ValidationEtensions
+{
+	public static bool Validate<TInput>(this TInput input, params Func<TInput, bool>[] predicates) =>
+		Array.TrueForAll(predicates, x => x(input));
+}
